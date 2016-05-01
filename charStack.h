45b8121d79fe5a90a8a657f11f52dbc85a9b7271
ahmed <ahmed@CYPHER>
2016-05-01 15:21:18 +0200

    charStack

---
+++ charStack.h
@@ -0,0 +1,14 @@
+/* char Stack implementation */
+typedef struct
+{
+    char items[MAXSIZE];
+    int top;
+}charStack;
+
+/* char Stack prototypes */
+void charStackInitialize(charStack *s); /* Initializing the Stack */
+int charStackIsEmpty(charStack *s); /* Checks if the stack is empty or not */
+int charStackIIsFull(charStack *s); /* Checks if the stack is full or not */
+char charStackTop(charStack *s); /* Returns the top element from the Stack */
+void charStackPush(charStack *s, char value); /* Push element into the Stack */
+char charStackPop(charStack *s); /* Pops the last element from the Stack */
