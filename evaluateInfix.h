cc5aa97e9ab3fd6a37b01b6e9e063da7abf7e3c6
ahmed <ahmed@CYPHER>
2016-05-03 22:37:00 +0200

    charStack

--- evaluateInfix.h
+++
@@ -1,9 +0,0 @@
-void DisplayMenu();
-double EvaluateInfix(string infix);
-string ConvertInfixToPostfix(string infix);
-bool IsMathOperator(char token);
-int OrderOfOperations(char token);
-stringLinkedList PostfixToList(string postfix);
-int GetTypeColumn(char& currentChar);
-double EvaluatePostfix(stringLinkedList* postfix);
-double Calculate(char token, double op1, double op2);
