2c309fa98a1213c8058f130e962979b853c04aa8
ahmed <ahmed@CYPHER>
2016-05-03 22:38:44 +0200

    doubleStack

---
+++ doubleStack.h
@@ -0,0 +1,28 @@
+#ifndef IOSTREAM_H
+#define IOSTREAM_H
+#include <iostream>
+using namespace std;
+#endif
+
+#ifndef MAX_SIZE
+#define MAX_SIZE
+#define MAXSIZE 100
+#endif
+
+
+/* double Stack implementation */
+typedef struct
+{
+    double items[MAXSIZE];
+    int top;
+}doubleStack;
+
+
+/* double Stack prototypes */
+void doubleStackInitialize(doubleStack *s); /* Initializing the Stack */
+int doubleStackIsEmpty(doubleStack *s); /* Checks if the stack is empty or not */
+int doubleStackIIsFull(doubleStack *s); /* Checks if the stack is full or not */
+double doubleStackTop(doubleStack *s); /* Returns the top element from the Stack */
+void doubleStackPush(doubleStack *s, double value); /* Push element into the Stack */
+double doubleStackPop(doubleStack *s); /* Pops the last element from the Stack */
+int doubleStackSize(doubleStack *s); /* Number of elements in the Stack */
